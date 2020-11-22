5228d7ac810f2b1f0b0377fb1fa873aaaf3778b8
Lolo_32 <lol.b@free.fr>
2020-11-22 18:16:30 +0100

    Initial support of mail parsing and extracting information
    Actually, only support plain content

---
+++ src/mail.rs
@@ -0,0 +1,157 @@
+use chrono::{DateTime, Utc};
+use std::collections::HashMap;
+
+#[derive(Hash, Eq, PartialEq)]
+enum Type {
+    Text,
+    Html,
+    Image,
+    Pdf,
+}
+
+pub struct Mail {
+    from: String,
+    to: Vec<String>,
+    date: DateTime<Utc>,
+    headers: Vec<String>,
+    data: HashMap<Type, (Option<String>, String)>,
+}
+
+impl Mail {
+    pub fn new(from: String, to: Vec<String>, data: String) -> Self {
+        let mut this = Self {
+            from,
+            to,
+            date: Utc::now(),
+            headers: Vec::default(),
+            data: HashMap::default(),
+        };
+
+        let mut headers = true;
+
+        let mut data_temp = String::new();
+
+        for line in data.lines() {
+            if headers {
+                if line != "" {
+                    if &line[..1] == " " {
+                        if let Some(prev_line) = this.headers.last_mut() {
+                            prev_line.push(' ');
+                            prev_line.push_str(line.trim_start());
+                            continue;
+                        }
+                    }
+                    this.headers.push(line.to_string());
+                } else {
+                    headers = false;
+                }
+            } else {
+                if !data_temp.is_empty() {
+                    data_temp.push_str("\r\n");
+                }
+                data_temp.push_str(line);
+            }
+        }
+        this.data.insert(Type::Text, (None, data_temp));
+
+        // Extract Date
+        for header in &this.headers {
+            if header.len() > 6 && &header[..6] == "Date: " {
+                let date_str = &header[6..];
+                let local_date = DateTime::parse_from_rfc2822(date_str).ok();
+                if let Some(local_date) = local_date {
+                    this.date = local_date.with_timezone(&Utc);
+                    break;
+                }
+            }
+        }
+
+        this
+    }
+
+    /**
+     * Retrieve mail date, either from the Date header or if not present from the reception time
+     */
+    pub fn get_date(&self) -> DateTime<Utc> {
+        self.date
+    }
+
+    /**
+     * Retrieve the content in text format
+     */
+    pub fn get_text(&self) -> String {
+        if let Some((_, text)) = self.data.get(&Type::Text) {
+            text.clone()
+        } else {
+            self.get_html()
+        }
+    }
+
+    /**
+     * Retrieve the content in html format
+     */
+    pub fn get_html(&self) -> String {
+        if let Some((_, html)) = self.data.get(&Type::Html) {
+            html.clone()
+        } else {
+            String::new()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::TimeZone;
+
+    const DATA_SIMPLE: &str = r"Date: Sun, 22 Nov 2020 01:58:23 +0100
+To: to@mail.com
+From: from@mail.com
+Subject: test Sun, 22 Nov 2020 01:58:23 +0100
+Message-Id: <20201122015818.087219@example.net>
+X-Mailer: swaks v20201014.0 jetmore.org/john/code/swaks/
+
+This is a test mailing
+
+
+";
+
+    #[test]
+    fn split_headers_body() {
+        let mail = Mail::new(
+            "from@example.com".to_string(),
+            vec!["to@example.com".to_string()],
+            DATA_SIMPLE.to_string(),
+        );
+
+        assert_eq!(mail.headers.len(), 6);
+        assert_eq!(mail.from, "from@example.com");
+        assert_eq!(mail.to.len(), 1);
+        assert_eq!(mail.to.get(0), Some(&"to@example.com".to_string()));
+        assert!(mail.data.contains_key(&Type::Text));
+        assert_eq!(
+            mail.data.get(&Type::Text),
+            Some(&(None, "This is a test mailing\r\n\r\n".to_string()))
+        );
+    }
+
+    #[test]
+    fn test_getting_datetime() {
+        let mail = Mail::new(String::default(), Vec::default(), DATA_SIMPLE.to_string());
+
+        let date = mail.get_date();
+        assert_eq!(date.timezone(), Utc);
+
+        let dt = Utc.ymd(2020, 11, 22).and_hms(0, 58, 23);
+        assert_eq!(date, dt);
+    }
+
+    #[test]
+    fn get_text() {
+        let mail = Mail::new(String::default(), Vec::default(), DATA_SIMPLE.to_string());
+
+        let text = mail.get_text();
+
+        assert_eq!(text, "This is a test mailing\r\n\r\n".to_string());
+    }
+}
